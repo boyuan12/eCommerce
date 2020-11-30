266c2ac81fed6062c38d917d1be5d11ffe507e9f
Boyuan Liu <boyuanliu6@yahoo.com>
2020-11-30 09:24:36 -0800

    added popular pages tracker

--- shopper/models.py
+++ shopper/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class PageView(models.Model):
+    item_id = models.UUIDField()
+    timestamp = models.DateTimeField(auto_now=True)
--- shopper/views.py
+++ shopper/views.py
@@ -1,8 +1,21 @@
+import datetime
 from django.shortcuts import render
 from seller.models import Item, ItemPicture
+from .models import PageView
 # from django.http import HttpRequest
 
 # Create your views here.
+def sort_item(queryset):
+    results = {}
+    for i in queryset:
+        try:
+            results[i.item_id] = results[i.item_id] + 1
+        except KeyError:
+            results[i.item_id] = 1
+    return results
+
+
+
 def index(request):
     if request.GET.get("q") is not None:
         query = request.GET.get("q")
@@ -20,15 +33,32 @@ def index(request):
             "q": query
         })
     else:
-        return render(request, "shopper/index.html")
+
+        current_hour = datetime.datetime.now().hour
+
+        try:
+            PageView.objects.filter(timestamp__hour=str(current_hour-1)).delete()
+        except:
+            PageView.objects.filter(timestamp__hour=str(current_hour)).delete()
+
+        popular_items = []
+        result = sort_item(PageView.objects.filter())
+        print(result)
+        for i in result:
+            item = Item.objects.get(item_id=i)
+            pic = ItemPicture.objects.filter(item_id=item.item_id)[0]
+            popular_items.append([item, result[i], pic])
+
+        return render(request, "shopper/index.html", {
+            "popular": popular_items
+        })
 
 
 def view_item(request, item_id):
     item = Item.objects.get(item_id=item_id)
     images = ItemPicture.objects.filter(item_id=item_id)
 
-    print(images)
-    print(images[1:])
+    PageView(item_id=item_id).save()
 
     return render(request, "shopper/view-item.html", {
         "item": item,
