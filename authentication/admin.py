b20b5b6107b94caccfb82711f318487a014cbd2b
Boyuan Liu <boyuanliu6@yahoo.com>
2023-10-06 04:44:37 +0000

    fixed model import

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, TWOFAVerified, TwoFAToken
 
 # Register your models here.
 admin.site.register(Profile)
--- seller/admin.py
+++ seller/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, StripeConnected
 
 # Register your models here.
 admin.site.register(Item)
