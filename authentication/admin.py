deb5ce7ede68f5204120542bb275fca8d692b739
Boyuan Liu <boyuanliu6@yahoo.com>
2023-10-06 04:42:48 +0000

    added more models to admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,3 +3,5 @@ from .models import Profile
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(TWOFAVerified)
+admin.site.register(TwoFAToken)
--- seller/admin.py
+++ seller/admin.py
@@ -3,3 +3,4 @@ from .models import Item
 
 # Register your models here.
 admin.site.register(Item)
+admin.site.register(StripeConnected)
